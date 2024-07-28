495d0aa4646d37c5e1f6864c13fcc5063ee3e5b4
llammann <bd7n2v9tr.code.edu.az>
2024-07-28 14:29:09 +0400

    handling error

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,9 +3,9 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     .filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = newGrades.find(
-        (grade) => grade.studentId === student.id
+        (grade) => grade.studentId === student.id,
       );
-      const grade = gradeObj ? gradeObj.grade : "N/A";
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
       return { ...student, grade };
     });
 }
