407f05d83a8dc7e9351be117d2b173253b31f90e
llammann <bd7n2v9tr@code.edu.az>
2024-08-23 16:11:16 +0000

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """  collect 10 random numbers & return the 10 rand numbers """
+    """  The coroutine collect 10 random numbers, then return the 10 random numbers"""
     return [i async for i in async_generator()]
