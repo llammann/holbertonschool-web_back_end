a1820d66678cc2fff9a01049d4277b82dc4a4ca8
llammann <bd7n2v9tr@code.edu.az>
2024-08-24 18:33:55 +0000

    List all documents in Python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """ Method for lists all docs """
+    if mongo_collection:
+        return mongo_collection.find()
+    else:
+        return []
