88bf2b32ac7653da583bfaffe6f93bd6cb9582ac
llammann <bd7n2v9tr@code.edu.az>
2024-08-24 16:58:53 +0000

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -12,7 +12,7 @@ def index_range(page: int, page_size: int) -> tuple:
     particular pagination parameters.
     """
 
-start_index = (page - 1) * page_size
+    start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
 
