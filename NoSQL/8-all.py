2767b030f0efa2b5ee19d15c5d5a299992eda0ed
llammann <bd7n2v9tr@code.edu.az>
2024-08-24 18:50:29 +0000

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """ lists all documents in a collection """
 
-from pymongo import MongoClient
-fetch_all_documents = __import__('8-all').list_all
+from typing import List, Dict
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    all_schools = fetch_all_documents(school_collection)
-    for school in all_schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+def list_all(collection) -> List[Dict]:
+    """ retrieves all documents from the specified collection """
+    if collection:
+        return list(collection.find())
+    return []
 
