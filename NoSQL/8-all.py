87db785f41765141b3685a5ad341f0c5144949cb
llammann <bd7n2v9tr@code.edu.az>
2024-08-24 18:55:22 +0000

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-""" lists all documents in a collection """
+""" Lists all documents in a collection """
 
 from typing import List, Dict
 
 def list_all(mongo_collection) -> List[Dict]:
-    """ retrieves all documents from the specified collection """
-    if collection:
+    """ Retrieves all documents from the specified collection.
+
+    Args:
+        mongo_collection: The pymongo collection object to query.
+
+    Returns:
+        A list of documents in the collection. Returns an empty list if no documents are found.
+    """
+    if mongo_collection:
         return list(mongo_collection.find())
     return []
 
