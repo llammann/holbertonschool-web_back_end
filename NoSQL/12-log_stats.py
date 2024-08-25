6dd4c0a3c4301070c96199bb25304d9eaf75aaf2
llammann <bd7n2v9tr@code.edu.az>
2024-08-25 11:32:49 +0000

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
-""" 11. Where can I learn Python? """
+""" 12. Log stats """
+from pymongo import MongoClient
 
 
-def schools_by_topic(mongo_collection, topic):
-    """ Returns the list of school having a specific topic """
-    return [item for item in mongo_collection.find({"topics": topic})]
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def log_stats(mongo_collection, option=None):
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
