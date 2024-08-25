e824e9ce58623b5beb975717d901f1227d84fef2
llammann <bd7n2v9tr@code.edu.az>
2024-08-25 09:34:34 +0000

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """ Task 1: Simple pagination """
 import csv
-import math
 from typing import List, Tuple
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
 
 class Server:
     """Server class to paginate a database of popular baby names."""
@@ -46,17 +50,4 @@ class Server:
             return []
 
         return dataset[start_idx:end_idx]
-        
-    def index_range(page: int, page_size: int) -> tuple:
-        """ return a tuple of size two containing a start and end index """
-        range = ()
-        if page == 1:
-            Start_index = 0
-            End_index = page * page_size
-            range = (Start_index, End_index)
-        else:
-            Start_index = (page - 1) * page_size
-            End_index = page * page_size
-            range = (Start_index, End_index)
-        return range
 
