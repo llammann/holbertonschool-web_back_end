e1797ac3e3a24f8788e31d0a198c057c4889f341
llammann <bd7n2v9tr@code.edu.az>
2024-08-25 10:59:49 +0000

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-"""  lists all documents in a collection """
+""" Write a Python function that lists all documents in a collection """
 
+from pymongo.collection import Collection
 
-def list_all(mongo_collection):
-    """ Method that will lists all docs """
-    if mongo_collection:
-        return mongo_collection.find()
-    else:
-        return []
+def list_all(mongo_collection: Collection):
+    """ Lists all documents in a collection.
+    
+    Args:
+        mongo_collection (Collection): The pymongo collection object.
+        
+    Returns:
+        List[dict]: A list of all documents in the collection.
+    """
+    return list(mongo_collection.find()) if mongo_collection else []
+
