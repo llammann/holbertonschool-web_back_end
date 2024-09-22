0fa5b995e914024ce8ffbeca048179ab4a2dfdf5
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 22:40:18 +0400

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,21 +4,33 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 const port = 1245;
 const app = express();
-module.exports = app;
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
+app.get('/students', async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-  countStudents(database).then((data) => {
-    res.end(data.join('\n'));
-  }).catch((error) => {
+
+  try {
+    const data = await countStudents(database);
+    const totalStudents = Object.values(data).reduce((acc, group) => acc + group.length, 0);
+    res.write(`Number of students: ${totalStudents}\n`);
+
+    for (const [field, group] of Object.entries(data)) {
+      const names = group.map((student) => student.firstname).join(', ');
+      res.write(`Number of students in ${field}: ${group.length}. List: ${names}\n`);
+    }
+    res.end();
+  } catch (error) {
     res.end(`${error.message}`);
-  });
+  }
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
