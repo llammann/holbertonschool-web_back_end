ac9e2c04c4d23704833ece0ef851db017c867f31
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 22:51:21 +0400

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,6 +4,7 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 const port = 1245;
 const app = express();
+module.exports = app;
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
@@ -15,14 +16,16 @@ app.get('/students', async (req, res) => {
   res.write('This is the list of our students\n');
 
   try {
-    const data = await countStudents(database);
-    const totalStudents = Object.values(data).reduce((acc, group) => acc + group.length, 0);
+    const groups = await countStudents(database);
+    const totalStudents = Object.values(groups).reduce((acc, group) => acc + group.length, 0);
+
     res.write(`Number of students: ${totalStudents}\n`);
 
-    for (const [field, group] of Object.entries(data)) {
+    for (const [field, group] of Object.entries(groups)) {
       const names = group.map((student) => student.firstname).join(', ');
       res.write(`Number of students in ${field}: ${group.length}. List: ${names}\n`);
     }
+
     res.end();
   } catch (error) {
     res.end(`${error.message}`);
@@ -32,5 +35,3 @@ app.get('/students', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-module.exports = app;
