f277ccd29429d3e9cfd500e1123fb6e355aedac6
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 21:40:15 +0400

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,33 +6,34 @@ const app = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url, true);
 
   if (reqUrl.pathname === '/') {
-    // When the URL path is '/'
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
   } else if (reqUrl.pathname === '/students') {
-    // When the URL path is '/students'
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
-    // Read and append student data from the database
+
     countStudents(process.argv[2])
       .then((studentsData) => {
-        res.write(studentsData); // Write the student data to the response
-        res.end(); // End the response after all data is written
+
+        res.write(`${studentsData}\n`);
+        res.end();
       })
       .catch((err) => {
-        res.write(err.message); // In case of error, write the error message
-        res.end(); // End the response
+        res.write(err.message);
+        res.end();
       });
   } else {
-    // For other paths
+
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
-// The server should listen on port 1245
+
 app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
