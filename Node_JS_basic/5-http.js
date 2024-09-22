402cbe740ff5fcf3d5ca273fbb96c20f20f3a9af
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 21:49:10 +0400

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,22 +6,36 @@ const port = 1245;
 const app = http.createServer(async (req, res) => {
   if (req.method === 'GET') {
     if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello Holberton School!');
     } else if (req.url === '/students') {
-      await countStudents(process.argv[2])
-        .then((val) => {
-          res.write('This is the list of our students\n');
-          res.write(`Number of students: ${val.arr.length}\n`);
-          res.write(`Number of students in CS: ${val.locateCS.length}. List: ${val.locateCS.join(', ')}\n`);
-          res.write(`Number of students in SWE: ${val.locateSWE.length}. List: ${val.locateSWE.join(', ')}\n`);
-          res.end();
-        })
-        .catch((err) => {
-          res.write('This is the list of our students\n');
-          res.end(err.message);
-        });
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('This is the list of our students\n');
+
+      try {
+        const val = await countStudents(process.argv[2]);
+        res.write(`Number of students: ${val.arr.length}\n`);
+        res.write(`Number of students in CS: ${val.locateCS.length}. List: ${val.locateCS.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${val.locateSWE.length}. List: ${val.locateSWE.join(', ')}\n`);
+      } catch (err) {
+        res.write('This is the list of our students\n');
+        res.end(err.message);
+        return;
+      }
+
+      res.end();
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
     }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method Not Allowed');
   }
 });
-app.listen(port);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
 module.exports = app;
