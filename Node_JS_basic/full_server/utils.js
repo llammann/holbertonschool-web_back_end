b5db6851a58e198c7061c8369aaef213b7176fd4
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 22:21:20 +0400

    Organize a complex HTTP server using Express

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+
+const readDatabase = async (filePath) => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const students = {};
+    
+    data.trim().split('\n').forEach((line) => {
+      const [firstname, , field] = line.split(',');
+      if (field) {
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+    });
+    
+    return students;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = { readDatabase };
