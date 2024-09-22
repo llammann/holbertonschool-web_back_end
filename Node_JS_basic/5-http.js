6b2f7fb328e2b704894643a6b6fef5a34f914915
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 21:17:28 +0400

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,46 +1,40 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+const url = require('url');
 
-const port = 1245;
+const app = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true);
 
-const app = http
-  .createServer(async (req, res) => {
-    if (req.url === '/') {
-      res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-      res.write('This is the list of our students\n');
+  if (reqUrl.pathname === '/') {
+    // When the URL path is '/'
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (reqUrl.pathname === '/students') {
+    // When the URL path is '/students'
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
 
-      await countStudents(process.argv[2])
-        .then((data) => {
-          const fields = Object.keys(data);
+    // Read and append student data from the database
+    countStudents(process.argv[2])
+      .then((studentsData) => {
+        res.write(studentsData);  // Write the student data to the response
+        res.end();  // End the response after all data is written
+      })
+      .catch((err) => {
+        res.write(err.message);  // In case of error, write the error message
+        res.end();  // End the response
+      });
+  } else {
+    // For other paths
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
 
-          const total = fields.reduce(
-            (acc, curr) => acc + data[curr].numStudents,
-            0,
-          );
-
-          res.write(`Number of students: ${total}\n`);
-
-          for (let i = 0; i < fields.length; i += 1) {
-            res.write(
-              `Number of students in ${fields[i]}: ${
-                data[fields[i]].numStudents
-              }. `,
-            );
-            res.write(`List: ${data[fields[i]].names.join(', ')}`);
-
-            if (i < fields.length - 1) {
-              res.write('\n');
-            }
-          }
-        })
-        .catch((err) => {
-          res.write(err.message);
-        })
-        .finally(() => {
-          res.end();
-        });
-    }
-  }).listen(port);
+// The server should listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
 module.exports = app;
