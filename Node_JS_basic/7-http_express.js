402b0eec76666cbbcc46ed8dce54448fcf254724
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 23:12:55 +0400

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -11,27 +11,14 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
+app.get('/students', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-
-  try {
-    const groups = await countStudents(database);
-    const totalStudents = Object.values(groups).reduce((acc, group) => acc + group.length, 0);
-
-    res.write(`Number of students: ${totalStudents}\n`);
-
-    for (const [field, group] of Object.entries(groups)) {
-      const names = group.map((student) => student.firstname).join(', ');
-      res.write(`Number of students in ${field}: ${group.length}. List: ${names}\n`);
-    }
-
-    res.end();
-  } catch (error) {
+  countStudents(database).then((data) => {
+    res.end(data.join('\n'));
+  }).catch((error) => {
     res.end(`${error.message}`);
-  }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+app.listen(port);
