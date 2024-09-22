8a9c5f170e19e57b1824625db1c0928e6958a4f9
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 22:35:58 +0400

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,83 +1,24 @@
 const express = require('express');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-function countStudents(filepath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filepath, 'utf-8', (err, res) => {
-      if (err) return reject(new Error('Cannot load the database'));
-
-      const headerArray = res.split(/\r?\n|\n/);
-      const headers = headerArray[0].split(',');
-
-      const dictList = [];
-      const noHeaderArray = headerArray.slice(1);
-      for (let i = 0; i < noHeaderArray.length; i += 1) {
-        const data = noHeaderArray[i].split(',');
-        if (data.length === headers.length) {
-          const row = {};
-          for (let j = 0; j < headers.length; j += 1) {
-            row[headers[j].trim()] = data[j].trim();
-          }
-          dictList.push(row);
-        }
-      }
-
-      let countCS = 0;
-      let countSWE = 0;
-      const studentsCS = [];
-      const studentsSWE = [];
-
-      dictList.forEach((element) => {
-        if (element.field === 'CS') {
-          countCS += 1;
-          studentsCS.push(element.firstname);
-        } else if (element.field === 'SWE') {
-          countSWE += 1;
-          studentsSWE.push(element.firstname);
-        }
-      });
-
-      const countStudents = countCS + countSWE;
-
-      return resolve({
-        countStudents,
-        countCS,
-        countSWE,
-        studentsCS,
-        studentsSWE,
-      });
-    });
-  });
-}
-
-const pathToDB = process.argv[2];
-const app = express();
+const database = process.argv[2];
 const port = 1245;
+const app = express();
+module.exports = app;
 
 app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  await countStudents(pathToDB)
-    .then(({
-      countStudents,
-      countCS,
-      countSWE,
-      studentsCS,
-      studentsSWE,
-    }) => {
-      const text = 'This is the list of our students\n';
-      const total = `Number of students: ${countStudents}\n`;
-      const CS = `Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}\n`;
-      const SWE = `Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`;
-      res.status(200).send(text + total + CS + SWE);
-    })
-    .catch(() => {
-      res.status(404).send('Cannot load the database');
-    });
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  countStudents(database).then((data) => {
+    res.end(data.join('\n'));
+  }).catch((error) => {
+    res.end(`${error.message}`);
+  });
 });
 
 app.listen(port);
-
-module.exports = app;
