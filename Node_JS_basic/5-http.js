4899083abf1f03e256b077fe3376268491118a08
llammann <bd7n2v9tr.code.edu.az>
2024-09-22 21:43:29 +0400

    fixing lint errros

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,19 +6,15 @@ const app = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url, true);
 
   if (reqUrl.pathname === '/') {
-
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
   } else if (reqUrl.pathname === '/students') {
-
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
-
     countStudents(process.argv[2])
       .then((studentsData) => {
-
         res.write(`${studentsData}\n`);
         res.end();
       })
@@ -27,12 +23,10 @@ const app = http.createServer((req, res) => {
         res.end();
       });
   } else {
-
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
-
 
 app.listen(1245, () => {
   console.log('Server is listening on port 1245');
